ccaa3fd4cd6661a08e863e259287e821e95b1adc
udagasan <u.dagasan@hotmail.com>
2017-12-27 08:55:17 +0300

    .

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public void a()
+        {
+            var list= new List().
+        }
+    }
+}
